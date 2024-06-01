655bd3c4823b12af27bbbe0d777e02f4c4f2b032
Feyza Nur Şahin <141928475+Feyzanrs@users.noreply.github.com>
2024-06-01 18:25:02 +0300

    The changes in the model layer were made.

--- internal/domain/model/object/dlr.go
+++ internal/domain/model/object/dlr.go
@@ -4,58 +4,58 @@ import (
 	"fmt"
 )
 
-// Dlr is a struct that represents the object of a dlr basic values.
-type Dlr struct {
-	DlrData   string    `json:"dlr_data"`   // DlrData is the data of the dlr.
-	DlrType   DlrType   `json:"dlr_type"`   // DlrType is the type of the dlr.
-	DlrStatus DlrStatus `json:"dlr_status"` // DlrStatus is the status of the dlr.
-	Tags      []string  `json:"tags"`       // Tags is the tags of the dlr.
+// Page is a struct that represents the object of a page basic values.
+type Page struct {
+	PageData   string     `json:"page_data"`   // PageData is the data of the page.
+	PageType   PageType   `json:"page_type"`   // PageType is the type of the page.
+	PageStatus PageStatus `json:"page_status"` // PageStatus is the status of the page.
+	Tags       []string   `json:"tags"`        // Tags is the tags of the page.
 }
 
-// NewDlr creates a new *Dlr.
-func NewDlr(dlrData string,
-	dlrType DlrType,
-	dlrStatus DlrStatus,
-	tags []string) *Dlr {
-	return &Dlr{
-		DlrData:   dlrData,
-		DlrType:   dlrType,
-		DlrStatus: dlrStatus,
-		Tags:      tags,
+// NewPage creates a new *Page.
+func NewPage(pageData string,
+	pageType PageType,
+	pageStatus PageStatus,
+	tags []string) *Page {
+	return &Page{
+		PageData:   pageData,
+		PageType:   pageType,
+		PageStatus: pageStatus,
+		Tags:       tags,
 	}
 }
 
-// NewEmptyDlr creates a new *Dlr with empty values.
-func NewEmptyDlr() *Dlr {
-	return &Dlr{
-		DlrData:   "",
-		DlrType:   DlrTypeNONE,
-		DlrStatus: DlrStatusNONE,
-		Tags:      []string{},
+// NewEmptyPage creates a new *Page with empty values.
+func NewEmptyPage() *Page {
+	return &Page{
+		PageData:   "",
+		PageType:   PageTypeNONE,
+		PageStatus: PageStatusNONE,
+		Tags:       []string{},
 	}
 }
 
-// String returns a string representation of the Dlr.
-func (s *Dlr) String() string {
-	return fmt.Sprintf("DlrData: %v, "+
-		"DlrType: %v, "+
-		"DlrStatus: %v, "+
+// String returns a string representation of the Page.
+func (s *Page) String() string {
+	return fmt.Sprintf("PageData: %v, "+
+		"PageType: %v, "+
+		"PageStatus: %v, "+
 		"Tags: %v",
-		s.DlrData,
-		s.DlrType,
-		s.DlrStatus,
+		s.PageData,
+		s.PageType,
+		s.PageStatus,
 		s.Tags)
 }
 
-// Equals returns true if the Dlr is equal to the other Dlr.
-func (s *Dlr) Equals(other *Dlr) bool {
-	if s.DlrData != other.DlrData {
+// Equals returns true if the Page is equal to the other Page.
+func (s *Page) Equals(other *Page) bool {
+	if s.PageData != other.PageData {
 		return false
 	}
-	if s.DlrType != other.DlrType {
+	if s.PageType != other.PageType {
 		return false
 	}
-	if s.DlrStatus != other.DlrStatus {
+	if s.PageStatus != other.PageStatus {
 		return false
 	}
 	for i := range s.Tags {
@@ -66,25 +66,25 @@ func (s *Dlr) Equals(other *Dlr) bool {
 	return true
 }
 
-// Clone returns a clone of the Dlr.
-func (s *Dlr) Clone() *Dlr {
-	return &Dlr{
-		DlrData:   s.DlrData,
-		DlrType:   s.DlrType,
-		DlrStatus: s.DlrStatus,
-		Tags:      s.Tags,
+// Clone returns a clone of the Page.
+func (s *Page) Clone() *Page {
+	return &Page{
+		PageData:   s.PageData,
+		PageType:   s.PageType,
+		PageStatus: s.PageStatus,
+		Tags:       s.Tags,
 	}
 }
 
-// IsEmpty returns true if the Dlr is empty.
-func (s *Dlr) IsEmpty() bool {
-	if s.DlrData != "" {
+// IsEmpty returns true if the Page is empty.
+func (s *Page) IsEmpty() bool {
+	if s.PageData != "" {
 		return false
 	}
-	if s.DlrType != DlrTypeNONE {
+	if s.PageType != PageTypeNONE {
 		return false
 	}
-	if s.DlrStatus != DlrStatusNONE {
+	if s.PageStatus != PageStatusNONE {
 		return false
 	}
 	if len(s.Tags) != 0 {
@@ -93,26 +93,26 @@ func (s *Dlr) IsEmpty() bool {
 	return true
 }
 
-// IsNotEmpty returns true if the Dlr is not empty.
-func (s *Dlr) IsNotEmpty() bool {
+// IsNotEmpty returns true if the Page is not empty.
+func (s *Page) IsNotEmpty() bool {
 	return !s.IsEmpty()
 }
 
-// Clear clears the Dlr.
-func (s *Dlr) Clear() {
-	s.DlrData = ""
-	s.DlrType = DlrTypeNONE
-	s.DlrStatus = DlrStatusNONE
+// Clear clears the Page.
+func (s *Page) Clear() {
+	s.PageData = ""
+	s.PageType = PageTypeNONE
+	s.PageStatus = PageStatusNONE
 	s.Tags = []string{}
 }
 
-// Validate validates the Dlr.
-func (s *Dlr) Validate() error {
+// Validate validates the Page.
+func (s *Page) Validate() error {
 	if s.IsEmpty() {
-		return ErrorDlrIsEmpty
+		return ErrorPageIsEmpty
 	}
-	if s.DlrData == "" {
-		return ErrorDlrDlrDataIsEmpty
+	if s.PageData == "" {
+		return ErrorPagePageDataIsEmpty
 	}
 	return nil
 }
