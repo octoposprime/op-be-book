ccab167f9d4b9e1798bfeb76145db69c74892792
ebrarkesici <ebrarkesici@gmail.com>
2024-06-04 12:30:45 +0300

    Replaced 'dlr' to 'page'

--- internal/application/infrastructure/port/repository/db.go
+++ internal/application/infrastructure/port/repository/db.go
@@ -13,12 +13,12 @@ type DbPort interface {
 	// SetLogger sets logging call-back function
 	SetLogger(LogFunc func(ctx context.Context, logData *pb_logging.LogData) (*pb_logging.LoggingResult, error))
 
-	// GetDlrsByFilter returns the dlrs that match the given filter.
-	GetDlrsByFilter(ctx context.Context, dlrFilter me.DlrFilter) (me.Dlrs, error)
+	// GetPagesByFilter returns the pages that match the given filter.
+	GetPagesByFilter(ctx context.Context, pageFilter me.PageFilter) (me.Pages, error)
 
-	// SaveDlr insert a new dlr or update the existing one in the database.
-	SaveDlr(ctx context.Context, dlr me.Dlr) (me.Dlr, error)
+	// SavePage insert a new page or update the existing one in the database.
+	SavePage(ctx context.Context, page me.Page) (me.Page, error)
 
-	// DeleteDlr soft-deletes the given dlr in the database.
-	DeleteDlr(ctx context.Context, dlr me.Dlr) (me.Dlr, error)
+	// DeletePage soft-deletes the given page in the database.
+	DeletePage(ctx context.Context, page me.Page) (me.Page, error)
 }
